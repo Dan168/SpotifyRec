72560e91d54b38133ee384fed9c014f54c17faf3
Dan168 <93716943+Dan168@users.noreply.github.com>
2023-02-07 23:10:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -27,5 +27,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 knn = neighbors.KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train, y_train)
 
+#Danceability,Energy,Loudness,Speechiness,Acousticness,Liveness,Tempo
 print(knn.predict([[0.83, 0.839,-6.985,0.0699,0.00201,0.0353,128.012]]))
 print(knn.score(X_test, y_test))
