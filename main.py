c268d0a4b488207c86237b6331ac400a3b63170a
Dan168 <93716943+Dan168@users.noreply.github.com>
2023-02-07 23:09:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ X = si.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-#Scaling seems to throw it off
 #scaler = StandardScaler()
 #X_train = scaler.fit_transform(X_train)
 #X_test = scaler.transform(X_test)
